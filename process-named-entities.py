3cc161236cb91da926e85a269123b38263f22095
Bruno Martins <bgmartins@users.noreply.github.com>
2018-02-26 10:06:05 +0000

    Update process-named-entities.py

--- process-named-entities.py
+++ process-named-entities.py
@@ -1,13 +1,54 @@
+from __future__ import unicode_literals
+
+import os
 import spacy
 import argparse
 import re
 import itertools
 from spacy import displacy
+from spacy.tokens import Doc, Span, Token
 from spacy.lang.es import Spanish
+from flashtext import KeywordProcessor
 
 parser = argparse.ArgumentParser(description='Process text files and recognize named entities.')
 parser.add_argument('files', metavar='FILE', type=str, nargs='+', help='an input file to be processed')
 parser.add_argument('--concordance', dest='concordance', action='store_const', const=1, default=0, help='return concordances for the named entities (default: output HTML annotations)')
+
+class Entity(object):
+
+    def __init__(self, nlp, keywords_list=[], keywords_dict={}, keywords_file=None, label='', attrs=('has_entities', 'is_entity', 'entity_desc', 'entities')):
+        self._has_entities, self._is_entity, self._entity_desc, self._entities = attrs
+        self.keyword_processor = KeywordProcessor()
+        self.keyword_processor.add_keywords_from_list(keywords_list)
+        self.keyword_processor.add_keywords_from_dict(keywords_dict)
+        if keywords_file: self.keyword_processor.add_keyword_from_file(keywords_file)
+        self.label = label
+        Doc.set_extension(self._has_entities, getter=self.has_entities)
+        Doc.set_extension(self._entities, getter=self.iter_entities)
+        Span.set_extension(self._has_entities, getter=self.has_entities)
+        Span.set_extension(self._entities, getter=self.iter_entities)
+        Token.set_extension(self._is_entity, default=False)
+        Token.set_extension(self._entity_desc, getter=self.get_entity_desc)
+
+    def __call__(self, doc):
+        matches = self.keyword_processor.extract_keywords(doc.text, span_info=True)
+        spans = []  # keep spans here to merge them later
+        for _, start, end in matches:
+            entity = doc.char_span(start, end, label=self.label)
+            for token in entity: token._.set(self._is_entity, True)
+            spans.append(entity)
+            doc.ents = list(doc.ents) + [entity]
+        for span in spans: span.merge()
+        return doc
+
+    def has_entities(self, tokens):
+        return any(token._.get(self._is_entity) for token in tokens)
+
+    def iter_entities(self, tokens):
+        return [(t.text, i, t._.get(self._entity_desc)) for i, t in enumerate(tokens) if t._.get(self._is_entity)]
+
+    def get_entity_desc(self, token):
+        return token.text
 
 args = parser.parse_args()
 nlp = spacy.load('es')
