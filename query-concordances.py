bd0cb545ea50ee59c5750be6fb943211031c6b7b
bgmartins <bgmartins@gmail.com>
2020-01-03 11:06:48 +0000

    Improvements on concordance tool.

--- query-concordances.py
+++ query-concordances.py
@@ -7,17 +7,18 @@ import spacy
 import argparse
 import re
 import itertools
+import pandas as pd
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.text import Text
 
-parser = argparse.ArgumentParser(description='Process concordance queries.')
-parser.add_argument('query', metavar='QUERY', type=str, nargs='+', help='An input text file to be processed')
-parser.add_argument('-i','--input', help='Input file name', required=True)
-parser.add_argument('-o','--output', help='Output file name', default="stdout")
-parser.add_argument('-l','--left-margin', help='Left context length', default=5)
-parser.add_argument('-r','--right-margin', help='Right context length', default=5)
+parser = argparse.ArgumentParser(description='a simple command line script to process concordance queries')
+parser.add_argument('query', metavar='QUERY', type=str, nargs='+', help='list of query terms')
+parser.add_argument('-i','--input', help='input file name', required=True)
+parser.add_argument('-o','--output', help='output file name', default="stdout")
+parser.add_argument('-l','--left-margin', help='left context length', default=5)
+parser.add_argument('-r','--right-margin', help='right context length', default=5)
 
-def n_concordance_tokenised(text,phrase,left_margin=5,right_margin=5):
+def n_concordance_tokenised(text,phrase,left_margin=1,right_margin=1):
     phraseList=phrase.split(' ')
     c = nltk.ConcordanceIndex(text.tokens, key = lambda s: s.lower())
     offsets=[c.offsets(x) for x in phraseList]
@@ -25,9 +26,16 @@ def n_concordance_tokenised(text,phrase,left_margin=5,right_margin=5):
     for i in range(len(phraseList)): offsets_norm.append([x-i for x in offsets[i]])
     intersects=set(offsets_norm[0]).intersection(*offsets_norm[1:])
     outputs = intersects
-    #concordance_txt = ([text.tokens[map(lambda x: x-left_margin if (x-left_margin) > 0 else 0,[offset])[0]:offset+len(phraseList)+right_margin] for offset in intersects])
-    #outputs=[''.join([x+' ' for x in con_sub]) for con_sub in concordance_txt]
-    return outputs
+    for offset in intersects:
+        concordance_txt_left = [ ]
+        concordance_txt_right = [ ]
+        concordance_txt_middle = [ ]
+        start_offset = offset - left_margin if (offset - left_margin) > 0 else 0
+        end_offset = offset + len(phraseList) + right_margin
+        for x in range(start_offset,offset): concordance_txt_left += [ text.tokens[x] ]
+        for x in range(offset + len(phraseList) ,end_offset): concordance_txt_right += [ text.tokens[x] ]
+        for x in range(offset , offset + len(phraseList)): concordance_txt_middle += [ text.tokens[x] ]        
+        yield ' '.join(concordance_txt_left), ' '.join(concordance_txt_middle), ' '.join(concordance_txt_right)
 
 def n_concordance(txt,phrase,left_margin=5,right_margin=5):
     tokens = nltk.word_tokenize(txt)
@@ -42,7 +50,11 @@ def main():
     tokens = word_tokenize(text)
     textList = Text(tokens)
     result =  n_concordance_tokenised(textList, args.query, left_margin=args.left_margin,right_margin=args.right_margin)
-    print(result)
-    
+    if args.output == "stdout": 
+        for r in result: print(r)
+    else:
+        output = open(args.output, "w")
+        pd.DataFrame(result).to_excel(args.output)
+
 if __name__ == '__main__':
     main()
